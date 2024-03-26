eb2b5c1a1d228b50d4a1d0d91d7e868b8f83c1c4
hchoi3 <hyunmin.choi@nih.gov>
2024-03-26 14:38:42 -0400

    change to using loop

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,8 @@ data "aws_instances" "ansible" {
 }
 
 resource "ansible_host" "host" {
-  name   = data.aws_instances.ansible.private_ips[0]  # Replace with the actual hostname or IP address of your host
+  for_each = data.aws_instances.ansible.private_ips
+  name   = each.key  # Replace with the actual hostname or IP address of your host
   groups = ["test"]  # Replace with the name of the group(s) this host belongs to in your Ansible inventory
 
   variables = {
