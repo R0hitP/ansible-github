7f0d9d46316aad658f1abf5796a84f596bb06229
hchoi3 <hyunmin.choi@nih.gov>
2024-03-26 14:16:20 -0400

    changed loco

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,9 @@
+data "aws_instances" "ansible" {
+  instance_tags = {
+    target = "test"
+  }
+}
+
 resource "ansible_host" "host" {
   name   = data.aws_instances.ansible.private_ips  # Replace with the actual hostname or IP address of your host
   groups = ["test"]  # Replace with the name of the group(s) this host belongs to in your Ansible inventory
@@ -6,11 +12,5 @@ resource "ansible_host" "host" {
     ansible_user        = "ansible",
     ansible_ssh_private_key_file = "~/.ssh/id_rsa",  # Replace with the path to your SSH private key
     ansible_python_interpreter = "/usr/bin/python3"  # Replace with the path to your Python interpreter
-  }
-}
-
-data "aws_instances" "ansible" {
-  instance_tags = {
-    target = "test"
   }
 }
