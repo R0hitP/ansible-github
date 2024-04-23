51862b50e3b272a7205a86b2209ec9dc30a1b952
hchoi3 <hyunmin.choi@nih.gov>
2024-04-23 10:40:53 -0400

    feat: public ip added

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,15 @@ resource "ansible_host" "host" {
     ansible_python_interpreter = "/usr/bin/python3"  # Replace with the path to your Python interpreter
   }
 }
+
+resource "ansible_host" "public_host" {
+  for_each = toset(data.aws_instances.ansible.public_ips)
+  name   = "${each.key}"  # Replace with the actual hostname or IP address of your host
+  groups = ["test"]  # Replace with the name of the group(s) this host belongs to in your Ansible inventory
+
+  variables = {
+    ansible_user        = "ansible",
+    ansible_ssh_private_key_file = "~/.ssh/id_rsa",  # Replace with the path to your SSH private key
+    ansible_python_interpreter = "/usr/bin/python3"  # Replace with the path to your Python interpreter
+  }
+}
