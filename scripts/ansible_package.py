1d7b67b8093efda6747606e81a9375032a6bfe0b
hchoi3 <hyunmin.choi@nih.gov>
2024-07-16 14:09:29 -0400

    assume role isntead

--- scripts/ansible_package.py
+++ scripts/ansible_package.py
@@ -3,16 +3,33 @@ import pandas as pd
 import yaml
 import os
 
-# Set the AWS profile to "trainee"
-os.environ['AWS_PROFILE'] = 'trainee'
+# Assume the role and get temporary credentials
+sts_client = boto3.client('sts')
+assumed_role_object = sts_client.assume_role(
+    RoleArn="arn:aws:iam::717192483375:role/cpe-admins-role",
+    RoleSessionName="AssumeRoleSession1"
+)
+
+# Extract the temporary credentials
+credentials = assumed_role_object['Credentials']
+access_key = credentials['AccessKeyId']
+secret_key = credentials['SecretAccessKey']
+session_token = credentials['SessionToken']
+
+# Create a new session using the temporary credentials
+assumed_role_session = boto3.Session(
+    aws_access_key_id=access_key,
+    aws_secret_access_key=secret_key,
+    aws_session_token=session_token
+)
+
+# Create the S3 client using the assumed role session
+s3_client = assumed_role_session.client('s3')
 
 # AWS S3 configuration
 bucket_name = 'aws-test-compliance-report-s3-bucket'
 filtered_key = 'inspector_finding/aws-test-inspector-report.csv'
 filtered_download_path = '/tmp/filtered-findings-report.csv'
-
-# Create the S3 client
-s3_client = boto3.client('s3')
 
 # Download the filtered CSV file from S3
 s3_client.download_file(bucket_name, filtered_key, filtered_download_path)
