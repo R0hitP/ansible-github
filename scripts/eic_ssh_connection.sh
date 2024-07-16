8337b30232d0d2743740a8ec77e798a90e410c27
hchoi3 <hyunmin.choi@nih.gov>
2024-07-16 14:56:30 -0400

    profile updated

--- scripts/eic_ssh_connection.sh
+++ scripts/eic_ssh_connection.sh
@@ -2,7 +2,7 @@
 
 csv_file="/tmp/filtered-findings-report.csv"
 awk  'BEGIN{ FS=",";OFS=","}{if (NR>1) print $1}' "$csv_file" | while read instanceId; do
-output=$(aws ec2-instance-connect send-ssh-public-key --instance-id ${instanceId} --region us-east-1 --instance-os-user ssm-user --ssh-public-key file://~/.ssh/id_rsa.pub --profile UserR)
+output=$(aws ec2-instance-connect send-ssh-public-key --instance-id ${instanceId} --region us-east-1 --instance-os-user ssm-user --ssh-public-key file://~/.ssh/id_rsa.pub --profile trainee)
 echo "$output"
 done
 
