3c0ff0aa1ebe7629f4d4eef71561e2cb97fc5540
hchoi3 <hyunmin.choi@nih.gov>
2024-07-16 14:54:46 -0400

    feat: added eic send public ssh key loop

---
+++ scripts/eic_ssh_connection.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+csv_file="/tmp/filtered-findings-report.csv"
+awk  'BEGIN{ FS=",";OFS=","}{if (NR>1) print $1}' "$csv_file" | while read instanceId; do
+output=$(aws ec2-instance-connect send-ssh-public-key --instance-id ${instanceId} --region us-east-1 --instance-os-user ssm-user --ssh-public-key file://~/.ssh/id_rsa.pub --profile UserR)
+echo "$output"
+done
+
+#need to remove duplicates
+#issue arise if there are instances that are not running in the csv
